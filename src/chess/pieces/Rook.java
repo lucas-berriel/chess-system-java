e50cfe35bc28f1b5702329fa45218ad33c9b2e10
Lucas Berriel <lucasberriel7788@gmail.com>
2023-04-24 10:23:16 -0400

    adding pawn

--- src/chess/pieces/Rook.java
+++ src/chess/pieces/Rook.java
@@ -11,11 +11,6 @@ public class Rook extends ChessPiece {
 	}
 
 	@Override
-	public String toString() {
-		return "R";
-	}
-
-	@Override
 	public boolean[][] possibleMoves() {
 		boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
 		Position p = new Position(0, 0);
@@ -61,5 +56,10 @@ public class Rook extends ChessPiece {
 		}
 
 		return mat;
+	}
+
+	@Override
+	public String toString() {
+		return "R";
 	}
 }
